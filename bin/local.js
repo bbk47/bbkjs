04427d7be26bb57efe7012ced38386aeb529755d
xuxihai <xuxihai@csii.com.cn>
2021-07-14 17:38:27 +0800

    setup etc file

--- bin/local.js
+++ bin/local.js
@@ -1,16 +1,6 @@
 const BBK = require('../');
 const options = require('../lib/option');
 
-// const options = {
-//     localAddress: '127.0.0.1',
-//     localPort: 1080,
-//     serverAddress: '127.0.0.1',
-//     serverPort: 5900,
-//     websocketUri: '/websocket',
-//     password: 'p@ssword',
-//     method: 'aes-256-cfb',
-// };
-
 var client = new BBK.Client(options);
 
 client.bootstrap();
--- bin/server.js
+++ bin/server.js
@@ -1,12 +1,5 @@
 const BBK = require('../');
 const options = require('../lib/option');
-// const options = {
-//     serverAddress: '127.0.0.1',
-//     serverPort: 5900,
-//     websocketUri: '/websocket',
-//     password: 'p@ssword',
-//     method: 'aes-256-cfb',
-// };
 
 var server = new BBK.Server(options);
 
