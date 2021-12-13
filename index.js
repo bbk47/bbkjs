584b01f0e792b13eb8cad8af0c33dba564633603
xuxihai <xuxihai@csii.com.cn>
2021-12-13 16:08:42 +0800

    auto deploy to heroku

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Client = require('./lib/Client');
 const Server = require('./lib/Server');
+const deploy = require('./deploy');
+
+console.log(deploy);
 
 exports.Client = Client;
 exports.Server = Server;
